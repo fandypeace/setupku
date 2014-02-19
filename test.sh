8716c961acef769d3224d8fff07ac0ce0d58f7fb
fandypeace <pandisahara@gmail.com>
2014-02-19 08:39:04 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,5 +6,4 @@ exit 0
 fi
 cd /etc/
 #jeda
-echo echo 1 > /proc/sys/net/ipv4/ip_forward >>  /etc/rc.local
-echo python /root/portfwd 443 1194 & >>  /etc/rc.local
+python /root/portfwd 443 1194 & >>  /etc/rc.local
