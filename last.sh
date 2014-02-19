6c8dddaddac2527d58a7ae173037d3101d15801a
fandypeace <pandisahara@gmail.com>
2014-02-19 09:32:49 +0700

    Create last.sh

---
+++ last.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#a
+cd root
+wget https://github.com/fandypeace/setupku/raw/master/webminku.sh
+sh webminku.sh
+#b
+wget https://github.com/fandypeace/setupku/raw/master/squidku.sh
+sh squidku.sh
+#c
+wget https://github.com/fandypeace/setupku/raw/master/greenvpn.sh
+sh greenvpn.sh
+#d
+wget https://github.com/fandypeace/setupku/raw/master/finishing.sh
+sh finishing.sh
