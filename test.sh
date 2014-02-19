c614121b3cd51119b8429fa7c198b1e2d732ca7e
fandypeace <pandisahara@gmail.com>
2014-02-19 08:52:49 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,10 +4,12 @@ then
 echo "Gunakan User root Untuk Menjalankan Script ini"
 exit 0
 fi
+#downLOAD
+wget 
 cd /etc/
 cp rc.local /etc/rc.local.fandy
 rm rc.local
 wget https://github.com/fandypeace/setupku/raw/master/rc.local
 
 #jeda
-nano /etc/rc.local
+reboot
